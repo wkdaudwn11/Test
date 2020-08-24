357ebcfbea2dc9ee0f24fc44e84bc52d3b54ff98
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-08-24 13:00:18 +0900

    test

--- server/src/controllers/testController.js
+++ server/src/controllers/testController.js
@@ -5,19 +5,19 @@ module.exports = {
     const { ip, country_name, country_capital } = req.body.browserInfo;
     await log(`api2 - test1: ${ip} / ${country_name} / ${country_capital}`);
     res.status(200).json({
-      test1: "test1",
+      test1: "api2 - test1",
     });
   },
   test2: async (req, res) => {
     const { ip, country_name, country_capital } = req.body.browserInfo;
-    await log(`test2: ${ip} / ${country_name} / ${country_capital}`);
+    await log(`api2 - test2: ${ip} / ${country_name} / ${country_capital}`);
     res.status(200).json({
       test2: "api2 - test2",
     });
   },
   test3: async (req, res) => {
     const { ip, country_name, country_capital } = req.body.browserInfo;
-    await log(`test3: ${ip} / ${country_name} / ${country_capital}`);
+    await log(`api2 - test3: ${ip} / ${country_name} / ${country_capital}`);
     res.status(200).json({
       test3: "api2 - test3",
     });
