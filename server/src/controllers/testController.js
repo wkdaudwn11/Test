57a88bf4ce707906cc8d37e1fd6804bc18a695ac
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-08-24 11:15:20 +0900

    test

--- server/src/controllers/testController.js
+++ server/src/controllers/testController.js
@@ -2,21 +2,21 @@ import { log } from "../commons/logger";
 
 module.exports = {
   test: async (req, res) => {
-    const { ip, country_name, country_capital } = req.body;
+    const { ip, country_name, country_capital } = req.body.browserInfo;
     await log(`test1: ${ip} / ${country_name} / ${country_capital}`);
     res.status(200).json({
       test1: "test1",
     });
   },
   test2: async (req, res) => {
-    const { ip, country_name, country_capital } = req.body;
+    const { ip, country_name, country_capital } = req.body.browserInfo;
     await log(`test2: ${ip} / ${country_name} / ${country_capital}`);
     res.status(200).json({
       test2: "test2",
     });
   },
   test3: async (req, res) => {
-    const { ip, country_name, country_capital } = req.body;
+    const { ip, country_name, country_capital } = req.body.browserInfo;
     await log(`test3: ${ip} / ${country_name} / ${country_capital}`);
     res.status(200).json({
       test3: "test3",
